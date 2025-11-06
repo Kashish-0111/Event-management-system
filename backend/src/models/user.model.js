3233c5e9ae2229489daf969a44e5879ea82655f9
Kashish-0111 <kashishdhankar57@gmail.com>
2025-11-06 16:08:20 +0530

    feat: add user model with password hashing, token generation, and environment variable integration

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,9 +1,12 @@
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken'
 import mongoose from 'mongoose';
 const userSchema= new mongoose.Schema({
     username:{
         type:String,
         required:true,
         unique:true,
+        trim:true
     },
     email:{
         type:String,
@@ -21,5 +24,43 @@ const userSchema= new mongoose.Schema({
        default: 'user', 
     }
 },{timestamps:true})
+ 
 
+
+
+userSchema.pre('save', async function(next){
+    if(!this.isModified("password")) return next();
+    this.password=bcrypt.hash(this.password,10)
+    next();
+})
+
+userSchema.methods.isPasswordCorrect=async function (password){
+   return await  bcrypt.compare(password,this.password)
+}
+
+userSchema.methods.generateAccessToken=function(){
+    return jwt.sign({
+        _id:this._id,
+        role:this.role,
+        email:this.email,
+        username:this.username
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+    }
+)
+}
+
+userSchema.methods.generateRefreshToken=function(){
+     return jwt.sign({
+        _id:this._id,
+        
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+        expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+    }
+)
+}
 export const User = mongoose.model("User",userSchema);
